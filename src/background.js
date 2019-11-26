dc57a09124d7af6f11bffe292cd14c088b9c348d
Anton Boichev <aboichev@artnet.com>
2019-11-26 09:46:18 -0500

    init commit

---
+++ src/background.js
@@ -0,0 +1,52 @@
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log('The color is green.');
+  });
+});
+
+
+let timerState = { isRunning: false, isPaused: false };
+
+function startTimer(duration) {
+  chrome.browserAction.setBadgeBackgroundColor({color: '#3aa757'});
+  timerState.isRunning = true;
+  const timer = setInterval(() => {
+      let minutes = parseInt(duration / 60, 10)
+      let seconds = parseInt(duration % 60, 10);
+
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      var badgeText = minutes + ":" + seconds;
+
+      chrome.browserAction.setBadgeText({text: badgeText});
+
+      if (!timerState.isPaused && --duration < 0) {         
+          clearInterval(timer);
+          timerState = { isRunning: false, isPaused: false };
+          chrome.storage.sync.set({'timerState': timerState});
+          chrome.browserAction.setBadgeBackgroundColor({color: '#e8453c'});
+          chrome.runtime.sendMessage({ cmd: 'REFRESH_UI' });
+      }
+  }, 1000);  
+};
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+
+      if (request.cmd == "START_TIMER") {
+        startTimer(request.durationInSecs);
+        chrome.storage.sync.set({'timerState': timerState}, sendResponse(timerState));
+      }
+
+      if (request.cmd == "PAUSE_TIMER") {
+        timerState.isPaused = true;
+        chrome.storage.sync.set({'timerState': timerState}, sendResponse(timerState));
+      }
+
+      if (request.cmd == "RESUME_TIMER") {
+        timerState.isPaused = false;
+        chrome.storage.sync.set({'timerState': timerState}, sendResponse(timerState));
+      }
+  }
+);
---
+++ src/popup.js
@@ -0,0 +1,85 @@
+'use strict';
+
+let changeColor = document.getElementById('changeColorBtn');
+
+chrome.storage.sync.get('color', function(data) {
+  changeColor.style.backgroundColor = data.color;
+  changeColor.setAttribute('value', data.color);
+});
+
+
+changeColor.onclick = function(e) {
+  let color = e.target.value;
+  
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+    if (tabs[0].url.startsWith("chrome://")) {
+      return;
+    }
+
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+  });
+};
+
+function updateUI (data) {
+  chrome.extension.getBackgroundPage().console.log('Got data', data);
+
+  if (data === undefined || data === null) {
+    chrome.storage.sync.get(['timerState'], (storageData) => updateUI(storageData.timerState));
+  }
+  else {
+    timerState = data;
+  }
+
+  if (timerState.isPaused) {
+    timerBtn.innerHTML = 'Resume';
+  }
+
+  chrome.extension.getBackgroundPage().console.log('isRunning', timerState.isRunning, timerState.isPaused);
+
+  if (timerState.isRunning && !timerState.isPaused) {
+    timerBtn.innerHTML = 'Pause';
+  }
+
+  if (!timerState.isRunning) {
+    timerBtn.innerHTML = 'Start';
+  }
+}
+
+let timerState = {};
+
+const timerBtn = document.getElementById('timerBtn');
+
+timerBtn.onclick = function(e) {
+  chrome.extension.getBackgroundPage().console.log('Start Button clicked');
+
+  if (!timerState.isRunning && !timerState.isPaused) {
+    chrome.runtime.sendMessage({
+      cmd: 'START_TIMER',
+      durationInSecs: 10
+    }, (newState) => updateUI(newState));
+  }
+
+  if (timerState.isRunning && timerState.isPaused) {
+    chrome.runtime.sendMessage({
+      cmd: 'RESUME_TIMER',
+    }, (newState) => updateUI(newState));
+  }
+  
+  if (timerState.isRunning && !timerState.isPaused) {
+    chrome.runtime.sendMessage({
+      cmd: 'PAUSE_TIMER'
+    }, (newState) => updateUI(newState));
+  }
+};
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+
+  if (request.cmd == "REFRESH_UI") {    
+    updateUI();
+  }
+});
+
+updateUI();
